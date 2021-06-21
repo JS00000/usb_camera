d22a737396a4d3451d7e65e115cb550440062b35
JS00000 <winyangyuxin22@hotmail.com>
2021-06-21 21:02:55 +0800

    cv python

---
+++ scripts/camera2.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import cv2
+cap0 = cv2.VideoCapture(0)
+cap0.set(3, 640)
+cap0.set(4, 480)
+cap1 = cv2.VideoCapture(2)
+cap1.set(3, 640)
+cap1.set(4, 480)
+ret0, frame0 = cap0.read()
+assert ret0
+ret1, frame1 = cap1.read()
+assert ret1
+
+while True:
+    ret0, frame0 = cap0.read()
+    ret1, frame1 = cap1.read()
+    if not ret0:
+        print("ERROR cap0")
+        break
+    if not ret1:
+        print("ERROR cap1")
+        break
+    cv2.imshow('frame0', frame0)
+    cv2.imshow('frame1', frame1)
+    if cv2.waitKey(1) == ord('q'):
+        break
+cap0.release()
+cap1.release()
+cv2.destoryAllWindows()
