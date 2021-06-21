4e7d993db2b52ca74e7c31d4b9d365483254ad25
JS00000 <winyangyuxin22@hotmail.com>
2021-06-21 20:09:08 +0800

    multi-thread

--- src/opencv_video_camera_2.cpp
+++ src/opencv_video_camera_2.cpp
@@ -7,121 +7,126 @@
 #include <thread>
 #include <mutex>
 #include <atomic>
+#include <chrono>
 
 #include <sys/stat.h>
 
 using namespace cv;
 using namespace std;
 
+Mat frame1, frame2;
 
-// class VideoCaptureMT {
-// public:
-//     VideoCaptureMT(int index, int height=480, int width=640);
-//     VideoCaptureMT(std::string filePath, int height=480, int width=640);
-//     ~VideoCaptureMT();
+class VideoCaptureMT {
+public:
+    VideoCaptureMT(int index, int height=480, int width=640);
+    VideoCaptureMT(std::string filePath, int height=480, int width=640);
+    ~VideoCaptureMT();
     
-//     bool isOpened() {
-//         return m_IsOpen;
-//     }
-//     void release() {
-//         m_IsOpen = false;
-//     }
-//     int read(cv::Mat& frame);
+    bool isOpened() {
+        return m_IsOpen;
+    }
+    void release() {
+        m_IsOpen = false;
+    }
+    bool read(cv::Mat& frame);
 
-// private:
-//     void captureInit(int index, std::string filePath, int height, int width);
-//     void captureFrame();
+private:
+    void captureInit(int index, std::string filePath, int height, int width);
+    void captureFrame();
 
-//     bool m_pFrame_next;
-//     cv::VideoCapture* m_pCapture;
-//     cv::Mat* m_pFrame;
-//     std::mutex* m_pMutex;
-//     std::thread* m_pThread;
-//     std::atomic_bool m_IsOpen;
-// };
+    int m_index;
+    bool m_pFrame_next;
+    cv::VideoCapture* m_pCapture;
+    cv::Mat* m_pFrame;
+    std::mutex* m_pMutex;
+    std::thread* m_pThread;
+    std::atomic_bool m_IsOpen;
+};
 
-// VideoCaptureMT::VideoCaptureMT(int index, int height, int width)
-// {
-//     captureInit(index, std::string(), height, width);
-// }
+VideoCaptureMT::VideoCaptureMT(int index, int height, int width)
+{
+    captureInit(index, std::string(), height, width);
+}
 
-// VideoCaptureMT::VideoCaptureMT(std::string filePath, int height, int width)
-// {
-//     captureInit(0, filePath, height, width);
-// }
+VideoCaptureMT::VideoCaptureMT(std::string filePath, int height, int width)
+{
+    captureInit(0, filePath, height, width);
+}
 
-// VideoCaptureMT::~VideoCaptureMT()
-// {
-//     m_IsOpen = false;
-//     m_pThread->join();
-//     if (m_pCapture->isOpened()) {
-//         m_pCapture->release();
-//     }
+VideoCaptureMT::~VideoCaptureMT()
+{
+    m_IsOpen = false;
+    m_pThread->join();
+    if (m_pCapture->isOpened()) {
+        m_pCapture->release();
+    }
 
-//     delete m_pThread;
-//     delete m_pMutex;
-//     delete m_pCapture;
-//     delete m_pFrame;
-// }
+    delete m_pThread;
+    delete m_pMutex;
+    delete m_pCapture;
+    delete m_pFrame;
+}
 
-// void VideoCaptureMT::captureInit(int index, std::string filePath, int height, int width)
-// {
-//     if (!filePath.empty()) {
-//         m_pCapture = new cv::VideoCapture(filePath);
-//     }
-//     else {
-//         m_pCapture = new cv::VideoCapture(index);
-//     }
-//     m_pCapture->set(cv::CAP_PROP_FRAME_WIDTH, width);
-//     m_pCapture->set(cv::CAP_PROP_FRAME_HEIGHT, height);
-//     m_pCapture->set(cv::CAP_PROP_FPS, 30);
+void VideoCaptureMT::captureInit(int index, std::string filePath, int height, int width)
+{
+    m_index = index;
+    if (!filePath.empty()) {
+        m_pCapture = new cv::VideoCapture(filePath);
+    }
+    else {
+        m_pCapture = new cv::VideoCapture(m_index, cv::CAP_V4L2);
+    }
+    m_pCapture->set(cv::CAP_PROP_FRAME_WIDTH, width);
+    m_pCapture->set(cv::CAP_PROP_FRAME_HEIGHT, height);
+    m_pCapture->set(cv::CAP_PROP_FPS, 30);
 
-//     cout << "camera " << index << endl;
-//     cout << "Frame width: " << m_pCapture->get(CAP_PROP_FRAME_WIDTH) << endl;
-//     cout << "     height: " << m_pCapture->get(CAP_PROP_FRAME_HEIGHT) << endl;
-//     cout << "Capturing FPS: " << m_pCapture->get(CAP_PROP_FPS) << endl;
+    cout << "camera " << m_index << endl;
+    cout << "Frame width: " << m_pCapture->get(CAP_PROP_FRAME_WIDTH) << endl;
+    cout << "     height: " << m_pCapture->get(CAP_PROP_FRAME_HEIGHT) << endl;
+    cout << "Capturing FPS: " << m_pCapture->get(CAP_PROP_FPS) << endl;
 
-//     m_IsOpen = true;
-//     m_pFrame_next = false;
-//     m_pFrame = new cv::Mat(height, width, CV_8UC3);
-//     m_pMutex = new std::mutex();
-//     m_pThread = new std::thread(&VideoCaptureMT::captureFrame, this);
-// }
+    m_IsOpen = true;
+    m_pFrame_next = false;
+    m_pFrame = new cv::Mat(height, width, CV_8UC3);
+    m_pMutex = new std::mutex();
+    m_pThread = new std::thread(&VideoCaptureMT::captureFrame, this);
+}
 
-// void VideoCaptureMT::captureFrame()
-// {
-//     cv::Mat frameBuff;
-//     while (m_IsOpen) {
-//         (*m_pCapture) >> frameBuff;
-//         if (m_pMutex->try_lock()) {
-//             frameBuff.copyTo(*m_pFrame);
-//             m_pFrame_next = true;
-//             m_pMutex->unlock();
-//         }
-//         cv::waitKey(5);
-//     }
-// }
+void VideoCaptureMT::captureFrame()
+{
+    while (m_IsOpen) {
+        m_pMutex->lock();
+        (*m_pCapture) >> (*m_pFrame);
+        m_pFrame_next = true;
+        m_pMutex->unlock();
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    }
+}
 
-// int VideoCaptureMT::read(cv::Mat& frame)
-// {
-//     if (m_pFrame->empty()) {
-//         m_IsOpen = false;
-//         return -1;
-//     } else if (m_pFrame_next) {
-//         m_pMutex->lock();
-//         m_pFrame->copyTo(frame);
-//         m_pFrame_next = false;
-//         m_pMutex->unlock();
-//         return 1;
-//     }
-//     return 0;
-// }
+bool VideoCaptureMT::read(cv::Mat& frame)
+{
+    if (m_pFrame->empty()) {
+        m_IsOpen = false;
+    } else {
+        while (1) {
+            if (m_pFrame_next) {
+                m_pMutex->lock();
+                m_pFrame->copyTo(frame);
+                m_pFrame_next = false;
+                m_pMutex->unlock();
+                break;
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        }
+    }
+    return m_IsOpen;
+}
 
 int main(int argc, char** argv)
 {
     int deviceID1 = 0;
     int deviceID2 = 1;
-    int apiID = cv::CAP_ANY;
+    int apiID = cv::CAP_V4L2;
     string save_path = "/dev/shm/";
     if (argc == 3) {
         deviceID1 = atoi(argv[1]);
@@ -133,8 +138,8 @@ int main(int argc, char** argv)
         return 1;
     }
     cout << "Opening camera..." << endl;
-    VideoCapture capture1(deviceID1); // open the first camera
-    VideoCapture capture2(deviceID2); // open the second camera
+    VideoCaptureMT capture1(deviceID1); // open the first camera
+    VideoCaptureMT capture2(deviceID2); // open the second camera
     if (!capture1.isOpened())
     {
         cerr << "ERROR: Can't initialize camera capture1" << endl;
@@ -146,27 +151,26 @@ int main(int argc, char** argv)
         return 1;
     }
 
+    // VideoWriter writer;
+    // int codec = VideoWriter::fourcc('M', 'J', 'P', 'G');
+    // capture1.set(CAP_PROP_FOURCC,codec);
+    // capture1.set(CAP_PROP_FRAME_WIDTH, 640);
+    // capture1.set(CAP_PROP_FRAME_HEIGHT, 480);
+    // capture1.set(CAP_PROP_FPS, 30.0);
+    // capture2.set(CAP_PROP_FOURCC,codec);
+    // capture2.set(CAP_PROP_FRAME_WIDTH, 640);
+    // capture2.set(CAP_PROP_FRAME_HEIGHT, 480);
+    // capture2.set(CAP_PROP_FPS, 30.0);
 
-    VideoWriter writer;
-    int codec = VideoWriter::fourcc('M', 'J', 'P', 'G');
-    capture1.set(CAP_PROP_FOURCC,codec);
-    capture1.set(CAP_PROP_FRAME_WIDTH, 640);
-    capture1.set(CAP_PROP_FRAME_HEIGHT, 480);
-    capture1.set(CAP_PROP_FPS, 30.0);
-    capture2.set(CAP_PROP_FOURCC,codec);
-    capture2.set(CAP_PROP_FRAME_WIDTH, 640);
-    capture2.set(CAP_PROP_FRAME_HEIGHT, 480);
-    capture2.set(CAP_PROP_FPS, 30.0);
+    // cout << "camera " << deviceID1 << endl;
+    // cout << "Frame width: " << capture1.get(CAP_PROP_FRAME_WIDTH) << endl;
+    // cout << "     height: " << capture1.get(CAP_PROP_FRAME_HEIGHT) << endl;
+    // cout << "Capturing FPS: " << capture1.get(CAP_PROP_FPS) << endl;
 
-    cout << "camera " << deviceID1 << endl;
-    cout << "Frame width: " << capture1.get(CAP_PROP_FRAME_WIDTH) << endl;
-    cout << "     height: " << capture1.get(CAP_PROP_FRAME_HEIGHT) << endl;
-    cout << "Capturing FPS: " << capture1.get(CAP_PROP_FPS) << endl;
-
-    cout << "camera " << deviceID2 << endl;
-    cout << "Frame width: " << capture2.get(CAP_PROP_FRAME_WIDTH) << endl;
-    cout << "     height: " << capture2.get(CAP_PROP_FRAME_HEIGHT) << endl;
-    cout << "Capturing FPS: " << capture2.get(CAP_PROP_FPS) << endl;
+    // cout << "camera " << deviceID2 << endl;
+    // cout << "Frame width: " << capture2.get(CAP_PROP_FRAME_WIDTH) << endl;
+    // cout << "     height: " << capture2.get(CAP_PROP_FRAME_HEIGHT) << endl;
+    // cout << "Capturing FPS: " << capture2.get(CAP_PROP_FPS) << endl;
 
     cout << endl << "Press 'ESC' to quit, 'space' to toggle frame processing" << endl;
     cout << endl << "Start grabbing..." << endl;
@@ -176,16 +180,21 @@ int main(int argc, char** argv)
     system(mkdir_pack_left.c_str());
     system(mkdir_pack_right.c_str());
 
-    Mat frame1, frame2;
     size_t nFrames = 0;
-    int64 t0 = cv::getTickCount();
+    // int64 t0 = cv::getTickCount();
+    auto t0 = chrono::steady_clock::now();
     int save_id = 0;
     while (capture1.isOpened() && capture2.isOpened())
     {
-        capture1 >> frame1;
-        capture2 >> frame2;
-        if (frame1.empty() || frame2.empty())
-        {
+        // capture1 >> frame1;
+        // capture2 >> frame2;
+        // if (frame1.empty() || frame2.empty())
+        // {
+        //     cerr << "ERROR: Can't grab camera frame." << endl;
+        //     break;
+        // }
+
+        if (!capture1.read(frame1) || !capture2.read(frame2)) {
             cerr << "ERROR: Can't grab camera frame." << endl;
             break;
         }
@@ -194,16 +203,18 @@ int main(int argc, char** argv)
         if (nFrames % 10 == 0)
         {
             const int N = 10;
-            int64 t1 = cv::getTickCount();
+            // int64 t1 = cv::getTickCount();
+            auto t1 = chrono::steady_clock::now();
             cout << "Frames captured: " << cv::format("%5lld", (long long int)nFrames)
-                 << "    Average FPS: " << cv::format("%9.1f", (double)getTickFrequency() * N / (t1 - t0))
-                 << "    Average time per frame: " << cv::format("%9.2f ms", (double)(t1 - t0) * 1000.0f / (N * getTickFrequency()))
+                 << "    Average FPS: " << cv::format("%9.1f", 1000000.0 * N / chrono::duration_cast<chrono::microseconds>(t1 - t0).count())
+                 << "    Average time per frame: " << cv::format("%9.2f ms", chrono::duration_cast<chrono::microseconds>(t1 - t0).count()/1000.0/N)
                  << std::endl;
             t0 = t1;
         }
 
         imshow("left", frame1);
         imshow("right", frame2);
+
         int key = waitKey(1);
         if (key == 27/*ESC*/) 
         {
